0ab700fcc3902adc3a902357e7b2644aa22abb3f
ADRIANA DE MELO CASTRO <adriana.mcastro@senacsp.edu.br>
2025-06-27 22:12:44 -0300

    alteracoes banner e nossa historia

--- src/components/NossaHistoria/index.js
+++ src/components/NossaHistoria/index.js
@@ -1,6 +1,11 @@
+import Brand from '../Brand';
+import './style.css'
+
 function NossaHistoria() {
     return(
-        <></>
+        <section className="Nossahistoria atelie-nossa-historia-img rounded-lg relative w-full h-[850px] mb-[50px]">
+           <Brand />
+        </section>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,20 @@
-import { useMemo } from "react";
-import Card from "../components/Card";
+
 import Banner from "../components/Banner";
 import NossaHistoria from "../components/NossaHistoria";
 import Orcamento from "../components/Orcamento";
 
 
 function Home() {
-    const services = useMemo(() => [
-        {image: '/icons/icon_network.svg', title: 'Otimize sua infraestrutura', texto: 'Soluções avancadas de infraestrutura, gerencimaneto dos recursos de rede proporcionando eficiencia seguranca e escalabilidade'},
-        {image: '/icons/icon_network.svg', title: 'Restauração e Backup', texto: 'Soluções avancadas de infraestrutura, gerencimaneto dos recursos de rede proporcionando eficiencia seguranca e escalabilidade'},
-        {image: '/icons/icon_network.svg', title: 'Restauração e Backup', texto: 'Soluções avancadas de infraestrutura, gerencimaneto dos recursos de rede proporcionando eficiencia seguranca e escalabilidade'},
-        {image: '/icons/icon_network.svg', title: 'Restauração e Backup', texto: 'Soluções avancadas de infraestrutura, gerencimaneto dos recursos de rede proporcionando eficiencia seguranca e escalabilidade'},
-        {image: '/icons/icon_network.svg', title: 'Restauração e Backup', texto: 'Soluções avancadas de infraestrutura, gerencimaneto dos recursos de rede proporcionando eficiencia seguranca e escalabilidade'},
-        {image: '/icons/icon_network.svg', title: 'Restauração e Backup', texto: 'Soluções avancadas de infraestrutura, gerencimaneto dos recursos de rede proporcionando eficiencia seguranca e escalabilidade'},
-        
-      ],[])
+    
     return(
         <div className="relative mx-auto flex-grow">
-           <Banner />
-           <NossaHistoria />
+            <div className="w-full px-[100px]">
+            <Banner />
+            </div>
+            <div className="w-full px-[100px]">
+            <NossaHistoria />
+            </div>
+           
            <Orcamento/>
         </div>
     )
