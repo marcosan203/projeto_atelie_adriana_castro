185009014abbe47018f646c42fc13edab7665ec3
Marcos Aurélio <marcosan203@gmail.com>
2025-08-08 16:42:26 -0300

    Atualização do componente galeria

--- src/components/Galeria/index.js
+++ src/components/Galeria/index.js
@@ -2,14 +2,54 @@ import './styles.css';
 import imagem1 from '../../assets/vestidoVermelho.png'
 function Galeria() {
     return (
-        <section>
+        <section className='areaCards'>
+
             <div className='cards'>
-                <img className='cardPrincipal' src="galeria_fotos/vestidoVermelho/vestidoVermelho.png" />
+
+                <div className='card'>
+                    <img className='cardPrincipal' src="galeria_fotos/vestidoVermelho/vestidoVermelho.png" />
+                </div>
                 <div className='cardsLinha'>
-                    <img className='card' src='galeria_fotos/macaquinhoTropicalEstampado/macaquinhoTropicalEstampado.jpg' />
-                    <img className='card' src='galeria_fotos/conjuntoEleganciaUrbana/conjuntoEleganciaUrbana.jpg' />
+                    <div className="galeria-item">
+                        <img src='galeria_fotos/macaquinhoTropicalEstampado/macaquinhoTropicalEstampado.jpg' alt='' className="galeria-imagem" />
+                        <div className="galeria-overlay">
+                            <span className="galeria-texto">
+                                Macaquinho leve e moderno com estampa tropical vibrante em azul, 
+                                rosa e preto. Possui modelagem ajustada, cintura marcada,
+                                 alças largas e botões frontais brancos. Confortável, 
+                                 feminino e versátil, ideal para dias quentes, passeios e eventos casuais.
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="galeria-item">
+                        <img src='galeria_fotos/conjuntoEleganciaUrbana/conjuntoEleganciaUrbana.jpg' alt='' className="galeria-imagem" />
+                        <div className="galeria-overlay">
+                            <span className="galeria-texto">Teste</span>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div className='cards'>
+                <div className='cardsLinha'>
+                    <img className='cardImagem' src='galeria_fotos/vestidoEstampado/vestidoEstampado.png' />
+                    <img className='cardImagem' src='galeria_fotos/vestidoTubinhoPreto/vestidoTubinhoPreto.png' />
+                </div>
+                <img className='cardPrincipal' src="galeria_fotos/conjunto/conjunto.png" />
+            </div>
+            <div className='cards'>
+                <img className='cardPrincipal' src='galeria_fotos/vestidoJuninoElegante/vestidoJuninoElegante.png' />
+                <div className='cardsLinha'>
+                    <img className='cardImagem' src='galeria_fotos/camisolaBrancaComRenda/camisolaBrancaComRenda.png' />
+                    <img className='cardImagem' src='galeria_fotos/camisolaRoseVintage/camisolaRoseVintage.jpg' />
                 </div>
             </div>
+
+
         </section>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,11 @@
 import Banner from "../components/Banner";
 import NossaHistoria from "../components/NossaHistoria";
 import Orcamento from "../components/Orcamento";
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import Perguntas_Respostas from "../components/Perguntas_Respostas"
-=======
+///=======
 import Galeria from "../components/Galeria";
->>>>>>> f51f2949d08bbf10941d2862f42c2fd874005855
+//>>>>>>> f51f2949d08bbf10941d2862f42c2fd874005855
 
 
 function Home() {
@@ -16,25 +16,23 @@ function Home() {
             <div className="w-full px-[100px]">
                 <Banner />
             </div>
-            
+
             <div className="w-full px-[100px]">
                 <NossaHistoria />
             </div>
-<<<<<<< HEAD
-            
+
             <div className="w-full px-[100px]">
-            <Orcamento/>
+                <Orcamento />
             </div>
 
             <div className="w-full px-[100px]">
-            <Perguntas_Respostas/>
+                <Perguntas_Respostas />
             </div>
-=======
             <div className="w-full px-[100px]">
                 <Galeria />
             </div>
             <Orcamento />
->>>>>>> f51f2949d08bbf10941d2862f42c2fd874005855
+
         </div>
     )
 }
