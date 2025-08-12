bc94b2e6c1b3e755988a52276499713b163691d3
WESLEY FELIPE LIMA ANTUNES <wesley.flantunes@senacsp.edu.br>
2025-08-12 19:18:05 -0300

    perguntas respostas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,20 @@
 import Banner from "../components/Banner";
 import NossaHistoria from "../components/NossaHistoria";
 import Orcamento from "../components/Orcamento";
-<<<<<<< HEAD
-import Perguntas_Respostas from "../components/Perguntas_Respostas"
-=======
-import Galeria from "../components/Galeria";
->>>>>>> f51f2949d08bbf10941d2862f42c2fd874005855
+import Perguntas_Respostas from "../components/Perguntas_Respostas/index.jsx"
 
 
 function Home() {
-
-    return (
+    
+    return(
         <div className="relative mx-auto flex-grow">
             <div className="w-full px-[100px]">
-                <Banner />
+            <Banner />
             </div>
             
             <div className="w-full px-[100px]">
-                <NossaHistoria />
+            <NossaHistoria />
             </div>
-<<<<<<< HEAD
             
             <div className="w-full px-[100px]">
             <Orcamento/>
@@ -29,12 +24,6 @@ function Home() {
             <div className="w-full px-[100px]">
             <Perguntas_Respostas/>
             </div>
-=======
-            <div className="w-full px-[100px]">
-                <Galeria />
-            </div>
-            <Orcamento />
->>>>>>> f51f2949d08bbf10941d2862f42c2fd874005855
         </div>
     )
 }
