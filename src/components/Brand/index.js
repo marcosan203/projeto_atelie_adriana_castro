f13bef4cbbdf8121e3cbda0556521159013796a5
ADRIANA DE MELO CASTRO <adriana.mcastro@senacsp.edu.br>
2025-08-14 22:05:34 -0300

    Atualiza NossaHistoria: ajusta imagens, fonte e texto

--- src/components/Brand/index.js
+++ src/components/Brand/index.js
@@ -1,6 +1,6 @@
 function Brand() {
     return(
-        <a href="#home" className="Brand absolute text-[45px] tracking-[10px] leading-[1] -top-11 left-10">
+        <a href="#home" className="Brand absolute z-50 text-[45px] tracking-[10px] leading-[1] -top-11 left-10">
             <h2 className='text-black'>
                 Ateliê
             </h2>
